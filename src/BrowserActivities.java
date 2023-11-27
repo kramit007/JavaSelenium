05f9d64dbb1e92d110d03156bf1ad05783aece6b
Amit <amitkumar225176@gmail.com>
2023-11-27 23:04:07 +0530

    Note, BrowserActivities and Locators file added

---
+++ src/BrowserActivities.java
@@ -0,0 +1,28 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BrowserActivities {
+
+	public static void main(String[] args) {
+//		Invoking different Browsers
+		
+		//Chrome
+		//System.setProperty(webdriver.chrome.driver,"D:\\Eclips\\Chromedriver\\chromedriver"");
+		WebDriver driver = new ChromeDriver();		
+
+		//EnternetExplored
+//		System.setProperty("wedriver.edge.driver", "D:\\Eclips\\Chromedriver\\edgedriver");
+//		WebDriver driver = new EdgeDriver();
+				
+		//FireFox
+//		System.setProperty("wedriver.gecko.driver", "D:\\Eclips\\Chromedriver\\gockodriver");
+//		WebDriver driver = new FireFoxDriver();
+		
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");		
+		driver.navigate().to("https://rahulshettyacademy.com/AutomationPractice/");
+		driver.navigate().back();
+		driver.navigate().forward();
+	}
+
+}
