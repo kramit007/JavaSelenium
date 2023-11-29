a78ce97b776090ca0e64122e7d2603f113446d2f
Amit <amitkumar225176@gmail.com>
2023-11-29 20:17:28 +0530

    updated canlendar file

--- src/Calendar.java
+++ src/Calendar.java
@@ -15,7 +15,7 @@ public class Calendar {
 		List<WebElement> dates = driver.findElements(By.className("flatpickr-day "));
 		int no = dates.size();
 		for (int i = 0; i < no; i++) {
-			String text = driver.findElements(By.className("flatpickr-day ")).get(i).getText();
+			String text = driver.findElements(By.className("flatpickr-day ")).get(i).getText();   // handle date
 			if (text.equalsIgnoreCase("28")) {
 				driver.findElements(By.className("flatpickr-day ")).get(i).click();
 				break;
