07a9e2b559efe63723506f13251c424c9e7e8237
Amit <amitkumar225176@gmail.com>
2023-11-29 22:45:31 +0530

    Updated Calendar file

--- src/Calendar.java
+++ src/Calendar.java
@@ -2,6 +2,7 @@ import java.time.Duration;
 import java.util.List;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -13,19 +14,21 @@ public class Calendar {
 		driver.get("https://www.path2usa.com/travel-companion/");
 		driver.manage().window().maximize();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(4));
-		driver.findElement(By.xpath("//input[@id='form-field-travel_comp_date']")).click();
+		JavascriptExecutor js = (JavascriptExecutor)driver;
+		js.executeScript("window.scrollBy(0,1000)");
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//input[@id='form-field-travel_comp_date']")).click();		
 //		To handle month
-		while (!driver.findElement(By.cssSelector("[class='datepicker-days'] [class='datepicker-switch']")).getText().contains("May")) 
+		while (!driver.findElement(By.xpath("//span[@class='cur-month']")).getText().trim().contains("March")) 
 		{				
-			driver.findElement(By.cssSelector("[class='datepicker-days'] th[class='next']")).click();
+			driver.findElement(By.xpath("//span[@class='flatpickr-next-month']")).click();
 		}
-//		To handle data
-		List<WebElement> dates = driver.findElements(By.className("flatpickr-day "));
-		int no = dates.size();
-		for (int i = 0; i < no; i++) {
-			String text = driver.findElements(By.className("flatpickr-day ")).get(i).getText(); 
+//		To handle date
+		int count = driver.findElements(By.className("flatpickr-day")).size();		
+		for (int i = 0; i <count; i++) {
+			String text = driver.findElements(By.className("flatpickr-day")).get(i).getText(); 
 			if (text.equalsIgnoreCase("22")) {
-				driver.findElements(By.className("flatpickr-day ")).get(i).click();
+				driver.findElements(By.className("flatpickr-day")).get(i).click();
 				break;
 			}
 		}
